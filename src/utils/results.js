c2d96aa9cdab567cb6e858792b240da7cc142ef5
Sergio Galici <galici.sergio@gmail.com>
2024-06-22 08:41:25 +0200

    refactor: Update water ratio calculation for protein content

--- src/utils/results.js
+++ src/utils/results.js
@@ -19,9 +19,11 @@ export const calculateDough = (values, setResults, setModalVisible) => {
     if (flourKind === "farina di mais") {
       totalProtein += 0
     } else if (flourKind === "farina di farro") {
-      totalProtein += flourAmount * ((proteinContent * 0.85) / 100)
+      totalProtein += flourAmount * ((proteinContent * 0.75) / 100)
     } else if (flourKind === "farina di segale") {
-      totalProtein += flourAmount * ((proteinContent * 0.5) / 100)
+      totalProtein += flourAmount * ((proteinContent * 0.4) / 100)
+    } else if (flourKind === "grano duro") {
+      totalProtein += flourAmount * ((proteinContent * 0.85) / 100)
     } else {
       totalProtein += flourAmount * (proteinContent / 100)
     }
@@ -109,7 +111,7 @@ export const calculateDough = (values, setResults, setModalVisible) => {
 
   let wRating = ""
   let waterRangeMin, waterRangeMax
-  let waterRatio = 1 + totalFiber * 0.001
+  let waterRatio = 1 + fiberRatio / 100
 
   // Se totalFlourAmount è maggiore di zero, calcola la percentuale di grano duro e grano tenero
   const durumWheatPercentage = (totalDurumWheat / totalFlourAmount) * 100
@@ -125,7 +127,7 @@ export const calculateDough = (values, setResults, setModalVisible) => {
   console.log("Corn percentage:", cornPercentage)
 
   // Definisci i fattori di assorbimento acqua per ogni tipo di farina
-  const durumWheatWaterRatio = 0.95
+  const durumWheatWaterRatio = 0.9
   const softWheatWaterRatio = 1
   const ryeWaterRatio = 1.5
   const speltWaterRatio = 1.2
@@ -140,46 +142,51 @@ export const calculateDough = (values, setResults, setModalVisible) => {
       totalCorn * cornWaterRatio) /
     totalFlourAmount
 
+  waterRatio += totalFiber * 0.002
+
   console.log("Water ratio:", waterRatio)
   console.log("Protein ratio:", proteinRatio)
 
   if (proteinRatio < 10) {
-    waterRangeMin = totalFlourAmount * 0.55 * waterRatio
+    waterRangeMin = totalFlourAmount * 0.57 * waterRatio
     waterRangeMax = totalFlourAmount * 0.6 * waterRatio
     wRating = "90 - 220"
   } else if (proteinRatio >= 10 && proteinRatio < 11) {
-    waterRangeMin = totalFlourAmount * 0.6 * waterRatio
-    waterRangeMax = totalFlourAmount * 0.7 * waterRatio
+    waterRangeMin = totalFlourAmount * 0.61 * waterRatio
+    waterRangeMax = totalFlourAmount * 0.69 * waterRatio
     wRating = "160 - 240"
   } else if (proteinRatio >= 11 && proteinRatio < 12) {
-    waterRangeMin = totalFlourAmount * 0.6 * waterRatio
-    waterRangeMax = totalFlourAmount * 0.75 * waterRatio
+    waterRangeMin = totalFlourAmount * 0.63 * waterRatio
+    waterRangeMax = totalFlourAmount * 0.73 * waterRatio
     wRating = "220 - 260"
   } else if (proteinRatio >= 12 && proteinRatio < 13) {
-    waterRangeMin = totalFlourAmount * 0.6 * waterRatio
-    waterRangeMax = totalFlourAmount * 0.8 * waterRatio
+    waterRangeMin = totalFlourAmount * 0.65 * waterRatio
+    waterRangeMax = totalFlourAmount * 0.75 * waterRatio
     wRating = "240 - 290"
   } else if (proteinRatio >= 13 && proteinRatio < 14) {
-    waterRangeMin = totalFlourAmount * 0.65 * waterRatio
-    waterRangeMax = totalFlourAmount * 0.8 * waterRatio
+    waterRangeMin = totalFlourAmount * 0.69 * waterRatio
+    waterRangeMax = totalFlourAmount * 0.79 * waterRatio
     wRating = "270 - 340"
   } else if (proteinRatio >= 14 && proteinRatio < 15) {
-    waterRangeMin = totalFlourAmount * 0.7 * waterRatio
-    waterRangeMax = totalFlourAmount * 1 * waterRatio
+    waterRangeMin = totalFlourAmount * 0.72 * waterRatio
+    waterRangeMax = totalFlourAmount * 0.82 * waterRatio
     wRating = "320 - 430"
   } else if (proteinRatio >= 15 && proteinRatio < 16) {
     waterRangeMin = totalFlourAmount * 0.8 * waterRatio
-    waterRangeMax = Math.min(totalFlourAmount * 1, totalFlourAmount)
+    waterRangeMax = totalFlourAmount * 1 * waterRatio
     wRating = "360 - 400++"
   } else if (proteinRatio >= 16) {
     waterRangeMin = totalFlourAmount * 0.8 * waterRatio
-    waterRangeMax = Math.min(totalFlourAmount * 1, totalFlourAmount)
+    waterRangeMax = totalFlourAmount * 1 * waterRatio
     wRating = "400++"
   }
 
   setResults({
     totalFlourAmount: totalFlourAmount.toFixed(0),
-    totalWater: `${waterRangeMin.toFixed(0)} - ${waterRangeMax.toFixed(0)}`,
+    totalWater: `${waterRangeMin.toFixed(0)} - ${Math.min(
+      totalFlourAmount,
+      waterRangeMax
+    ).toFixed(0)}`,
     totalSalt: totalSalt.toFixed(0),
     totalYeast: totalYeast.toFixed(0),
     riseTime,
